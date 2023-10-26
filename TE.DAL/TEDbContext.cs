7768c76ceac298db7c489ec6092c37ab2d263d6e
rudzuan2008 <rudzuan@gmail.com>
2023-10-26 10:09:15 +0800

    Add files via upload
    Added TE.DAL

---
+++ TE.DAL/TEDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TE.DAL
+{
+    public partial class TEDbContext : DbContext
+    {
+        public TEDbContext(DbContextOptions<TEDbContext> options)
+                : base(options)
+        {
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
